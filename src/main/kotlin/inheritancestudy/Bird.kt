1b449f1f1df7bc161ae5180ce25edbbdbba74250
고나은 <gonaeunn@gmail.com>
2025-05-09 16:34:08 +0900

    오버라이딩

--- src/main/kotlin/inheritancestudy/Bird.kt
+++ src/main/kotlin/inheritancestudy/Bird.kt
@@ -4,19 +4,30 @@ package org.example.InheritanceStudy
 open class Bird(
     var name: String  // 생성자를 사용하는 경우
 ) {
-    fun fly() {
+    open fun fly() {   // fly를 오버라이딩 하기 위해 open 키워드 추가
         println("${name} 날아요~")
     }
 
 }
 
-// 생성자를 사용하는 경우, 자식객체에서도 Bird의 생성자를 호출하면서 그 값(name)을 전달해줘야 함
+// 상속에서 생성자를 사용하는 경우, 자식객체에서도 Bird의 생성자를 호출하면서 그 값(name)을 전달해줘야 함
 class Chicken(name: String) : Bird(name) {
 
 }
 
-class Sparrow(name: String) : Bird(name) {
+// 오버라이딩
+// 상속받은 부모 클래스의 메소드를 재설계 (자식 클래스의 개성 살리기)
+class Sparrow(
+    name: String,
+    val color: String
+) : Bird(name) {
 
+    override fun fly() {
+//        super객체는 부모의 객체를 의미하며 자동으로 생성됨
+//        즉 부모객체의 fly메소드를 부르는 행위임
+//        부모의 기능을 어느정도 활용하면서 오버라이딩 하고싶으면, super.fly()
+        println("${color}의 ${name}이 날아봅니다~ 짹짹!")
+    }
 }
 
 class Pigeon(name: String) : Bird(name) {
--- src/main/kotlin/inheritancestudy/Main.kt
+++ src/main/kotlin/inheritancestudy/Main.kt
@@ -1,10 +1,10 @@
 package org.example.InheritanceStudy
 
 fun main() {
-    val bird = Bird()
-    val chicken = Chicken()
-    val sparrow = Sparrow()
-    val pigeon = Pigeon()
+    val bird = Bird("새")
+    val chicken = Chicken("닭")
+    val sparrow = Sparrow("참새", "갈색")
+    val pigeon = Pigeon("비둘기")
 
     bird.fly()
     chicken.fly()
